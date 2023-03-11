431fbc921c9193f25761210a6c7cc12c72056b93
reccrea_ <wellmoon0@163.com>
2023-03-11 11:27:36 +0800

    refactor(custom): develop-standard
    项目初始化

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,23 @@ import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 import stylelitPlugin from 'vite-plugin-stylelint';
 
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), eslintPlugin(), stylelitPlugin()],
+	plugins: [
+		vue(),
+		eslintPlugin(),
+		stylelitPlugin(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()],
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()],
+		}),
+	],
 	server: {
 		host: 'localhost',
 		port: 7070,
